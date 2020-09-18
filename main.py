1e0e7342750ba1f5c73a5fd761db0bf57a1a603b
jeterg <jetergdev@gmail.com>
2020-09-18 14:21:19 -0400

    Input and listing

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ data=response.json()['results']
 def display(problem):
     if data[problem]['type']=='multiple':
         print(problem+1,": ",data[problem]['question'])
-        choices=[data[problem]['incorrect_answers'][0],data[problem]['incorrect_answers'][1],data[problem]['incorrect_answers'][2],data[problem]['correct_answer']]
+        choices=[data[problem]['incorrect_answers'][0],
+                 data[problem]['incorrect_answers'][1],
+                 data[problem]['incorrect_answers'][2],
+                 data[problem]['correct_answer']]
         random.shuffle(choices)
-        for choice in choices:
-            print("\t\t",choice)
-        # print(data[problem]['correct_answer'])
+        labledChoice=[['A',choices[0]],['B',choices[1]],['C',choices[2]],['D',choices[3]]]
+        for choice in labledChoice:
+            print("\t\t",choice[0],':',choice[1])
+        submission = input("Choice:")
     else:
         print(problem+1,": ",data[problem]['question'])
         print("\t\t",'True')
         print("\t\t",'False')
-        # print(data[problem]['correct_answer'])
 
-display(0)
-# for entry in data:
-#     print(entry['type'])
