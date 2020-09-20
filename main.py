bd607afd403e70720ad2f31093c514a4b55228da
jeterg <jetergdev@gmail.com>
2020-09-19 22:18:25 -0400

    structure change for choices

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import random
-response=requests.get("https://opentdb.com/api.php?amount=200")
+response=requests.get("https://opentdb.com/api.php?amount=50")
 data=response.json()['results']
 def display(problem):
     if data[problem]['type']=='multiple':
@@ -10,12 +10,20 @@ def display(problem):
                  data[problem]['incorrect_answers'][2],
                  data[problem]['correct_answer']]
         random.shuffle(choices)
-        labledChoice=[['A',choices[0]],['B',choices[1]],['C',choices[2]],['D',choices[3]]]
-        for choice in labledChoice:
-            print("\t\t",choice[0],':',choice[1])
-        submission = input("Choice:")
+        labledChoice={
+                'A': choices[0],
+                'B': choices[1],
+                'C': choices[2],
+                'D': choices[3]
+        }
+        for key,value in labledChoice.items():
+            print("\t\t",key,':',value)
+
+        # submission = input("Choice:")
+        # if submission.upper()
+        print(labledChoice['A'])
     else:
         print(problem+1,": ",data[problem]['question'])
         print("\t\t",'True')
         print("\t\t",'False')
-
+display(0)
